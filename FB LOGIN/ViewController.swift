7a4fa2be32b2f459170812c9746f57a3980090b0
21giulio21 <21giulio21@gmail.com>
2017-03-30 14:16:47 +0200

    Finito login

--- FB LOGIN/ViewController.swift
+++ FB LOGIN/ViewController.swift
@@ -1,18 +1,62 @@
-//
-//  ViewController.swift
-//  FB LOGIN
-//
-//  Created by Giulio Tavella on 30/03/17.
-//  Copyright © 2017 Giulio Tavella. All rights reserved.
-//
+// L' ACCOUNT DI FIREBASE è 22giulio22@gmail.com
 
 import UIKit
+import FBSDKLoginKit
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let loginButton = FBSDKLoginButton()
+        
+        view.addSubview(loginButton)
+        
+        // questo modo di fare layout è obsoleto quindi devo poi usare i costrain
+        loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        loginButton.delegate = self
+        loginButton.readPermissions = ["email", "public_profile"]
+     
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("LOGOUNT DA FB")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil
+        {
+        
+            print(error)
+            return
+        
+        }
+        
+        print("LOGIN CORRETTO")
+        
+        // permette di fare una richiesta a fb sul mio profilo: /me e mandarmi il nome, la mail e l'id
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start { (connection , results, erorr) in
+            if error != nil
+            {
+                print("erorre:",error)
+                return
+            }
+           // ora entro con i dati che ho ricevuto su firebase
+            let accessToken = FBSDKAccessToken.current()
+            guard let accessTokenString = accessToken?.tokenString else {return}
+            let credential = FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString )
+            FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+                if error != nil
+                {
+                    print("erorre:",error)
+                    return
+   
+                }
+                
+                
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Pods/Target Support Files/Pods-FB LOGIN/Pods-FB LOGIN-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FB_LOGINVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FB_LOGINVersionString[];
+
